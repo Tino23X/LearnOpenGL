68f482e3d13809dcf1d1a207eb660858215d3ca8
tion <tion_23@163..com>
2018-09-02 15:32:06 +0800

    Test

--- LearnOpenGL/Tutorial1.cpp
+++ LearnOpenGL/Tutorial1.cpp
@@ -1,14 +1,23 @@
-//你好 窗口
-
 #include "gl.h"
 
 int main()
 {
+	//--Init
 	glfwInit();
+
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+	//Todo 还有那些模式
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	//Test
+	//--Create window
+	GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	if (window == NULL)
+	{
+	}
+
+	
+
 	return 0;
 }
