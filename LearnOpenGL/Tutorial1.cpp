983cd408ebe9e4edab9826e47a884763a9cc99bf
tion <tion_23@163..com>
2018-09-02 16:07:27 +0800

    Hello window

--- LearnOpenGL/Tutorial1.cpp
+++ LearnOpenGL/Tutorial1.cpp
@@ -1,8 +1,11 @@
 #include "gl.h"
 
+//当窗口大小改变时的回调函数
+void frameBufferSizeCallback(GLFWwindow *window, int width, int height);
+
 int main()
 {
-	//--Init
+	//--Init GLFW
 	glfwInit();
 
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -15,9 +18,42 @@ int main()
 	GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
 	if (window == NULL)
 	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	//--Init GLAD
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
 	}
 
-	
+	//--Set viewport
+	glViewport(-500, -700, 800, 600);
+
+	//注册回调函数
+	glfwSetFramebufferSizeCallback(window, frameBufferSizeCallback);
+
+
+	//--Set Render loop
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	//终止函数
+	glfwTerminate();
+
 
 	return 0;
+}
+
+void frameBufferSizeCallback(GLFWwindow * window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+
 }
